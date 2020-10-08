e643669938e9558fe1bef48e0d6c394c3a7fa62f
Andy Chang <achang129@yahoo.com>
2020-10-08 18:05:21 -0400

    Minor Fix to Finish Transaction

--- java/src/main/java/com/techelevator/VendingMachineCLI.java
+++ java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -90,12 +90,14 @@ public class VendingMachineCLI {
 					    int nickels = Math.round((int)change/5);
 					    change=change%5;
 					    int pennies = Math.round((int)change/1);
+					    currentMoney = 0;
 	
 					    System.out.println("Dollars: " + dollars);
 					    System.out.println("Quarters: " + quarters);
 					    System.out.println("Dimes: " + dimes);
 					    System.out.println("Nickels: " + nickels);
 					    System.out.println("Pennies: " + pennies);
+					    System.out.println("Current Balance: " + currentMoney);
 					}
 				}
 			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
